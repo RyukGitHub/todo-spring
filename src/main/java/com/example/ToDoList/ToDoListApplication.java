5e984b1f7841dad7f72ee54eed259bb15709379f
ShashankNadikatla <shashank.nadikatla@phenom.com>
2024-08-04 21:25:00 +0530

    Build Success & Added MongoURI

--- src/main/java/com/example/ToDoList/ToDoListApplication.java
+++ src/main/java/com/example/ToDoList/ToDoListApplication.java
@@ -2,12 +2,13 @@ package com.example.ToDoList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 public class ToDoListApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ToDoListApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListApplication.class, args);
+    }
 
 }
